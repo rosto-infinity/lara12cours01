a08b619baed70e572da95b4383cb60610bbf97f1
ROSTAND LELE <rostoinfocus@gmail.com>
2025-06-28 10:30:03 +0100

    crud-products

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::middleware(['auth', 'verified','role:admin'])->group(function () {
 Route::middleware(['auth', 'verified','role:superadmin'])->group(function () {
         Route::get('/admin/users',[TestController::class,'admin'])->name('admin');
         Route::get('/superadmin/system',[TestController::class,'superadmin'])->name('superadmin');
+
+        //Products
+        Route::get('/products', [TestController::class, 'index'])->name('products.index');
+        Route::get('/products/create', [TestController::class, 'create'])->name('products.create');
+        Route::post('/products/store', [TestController::class, 'store'])->name('products.store');
+        Route::get('/products/edit/{id}', [TestController::class, 'edit'])->name('products.edit');
+        Route::put('/products/update/{id}', [TestController::class, 'update'])->name('products.update');
+        Route::delete('/products/delete/{id}', [TestController::class, 'delete'])->name('products.delete');
 });
 
 require __DIR__.'/settings.php';
