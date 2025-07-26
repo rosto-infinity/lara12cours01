ed25a100f78bb65c365cea6a23779cd92d7e4cda
ROSTAND LELE <rostoinfocus@gmail.com>
2025-07-26 18:19:07 +0100

    comments export excel

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -2,21 +2,25 @@
 
 namespace App\Exports;
 
-use App\Models\Product;
-use Illuminate\Contracts\View\View; // --Importe l'interface View pour le type de retour
-use Maatwebsite\Excel\Concerns\FromView; // ---Importe l'interface FromView pour l'exportation Excel
-
+use App\Models\Product; // -- Importe le modèle Product pour accéder aux données des produits
+use Illuminate\Contracts\View\View; // -- Importe l'interface View pour définir le type de retour de la méthode view()
+use Maatwebsite\Excel\Concerns\FromView; // -- Importe l'interface FromView pour permettre l'exportation de données vers Excel à partir d'une vue
 
 class ProductExport implements FromView
 {
-    
+    /**
+     * Méthode view() qui retourne une vue contenant les données à exporter.
+     *
+     * @return View
+     */
     public function view(): View
     {
+        // Retourne la vue 'SuperAdmin/Products/ExcelProducts' avec les données des produits
         return view(
-            'SuperAdmin/Products/ExcelProducts',
-        [
-       'products' => Product::all()
-        ]
+            'SuperAdmin/Products/ExcelProducts', // -- Chemin de la vue utilisée pour l'exportation
+            [
+                'products' => Product::all() // -- Récupère tous les produits depuis la base de données et les passe à la vue
+            ]
         );
     }
 }
